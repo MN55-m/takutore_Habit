4e3f1c3a597010ff120f05142d14a6127dcfe3a5
“MN55-m” <ch1bi.m1ka26@gmail.com>
2024-12-27 11:52:04 +0900

    新規アカウント登録とログイン

--- takutore_project/app/views.py
+++ takutore_project/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from app.forms import SignupForm, LoginForm
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -9,12 +12,36 @@ class PortfolioView(View):
 
 class SignupView(View):
     def get(self, request):
-        return render(request, "signup.html")
-
+        form = SignupForm()
+        return render(request, "signup.html", context={
+            "form":form
+        })
+    def post(self, request):
+        print(request.POST)
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        return render(request, "signup.html", context={
+            "form":form
+        })
+    
 class LoginView(View):
     def get(self, request):
         return render(request, "login.html")
+    
+    def post(self, request):
+        print(request.POST)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            login(request, form.user)
+            return redirect("home")
+        return render(request, "login.html", context={
+            "form":form
+        })
 
-class HomeView(View):
+class HomeView(LoginRequiredMixin, View):
+    login_url = "login"
     def get(self, request):
         return render(request, "home.html")
