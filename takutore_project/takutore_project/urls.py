8d698ce50924b248e2799d81ed6ad282785da59c
“MN55-m” <ch1bi.m1ka26@gmail.com>
2025-07-21 12:18:29 +0900

    URL名の変更

--- takutore_project/takutore_project/urls.py
+++ takutore_project/takutore_project/urls.py
@@ -7,15 +7,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', PortfolioView.as_view(), name="portfolio"),
-    path('takutore_project/signup/', SignupView.as_view(), name="signup"),
-    path('takutore_project/login/', LoginView.as_view(), name="login"),
-    path('takutore_project/home/', HomeView.as_view(), name="home"),
-    path('takutore_project/home/calendar/', include('app_calendar.urls')),
-    path('takutore_project/home/video/', include('app_video.urls')),
-    path('takutore_project/home/workout/', include('app_workout.urls')),
-    path('takutore_project/home/mypage/', MypageView.as_view(), name="mypage"),
-    path('takutore_project/mypage/edit/', MyPageEditView.as_view(), name='mypage_edit'),
-    path('takutore_project/mypage/password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('takutore_Habit/signup/', SignupView.as_view(), name="signup"),
+    path('takutore_Habit/login/', LoginView.as_view(), name="login"),
+    path('takutore_Habit/home/', HomeView.as_view(), name="home"),
+    path('takutore_Habit/home/calendar/', include('app_calendar.urls')),
+    path('takutore_Habit/home/video/', include('app_video.urls')),
+    path('takutore_Habit/home/workout/', include('app_workout.urls')),
+    path('takutore_Habit/home/mypage/', MypageView.as_view(), name="mypage"),
+    path('takutore_Habit/mypage/edit/', MyPageEditView.as_view(), name='mypage_edit'),
+    path('takutore_Habit/mypage/password_change/', PasswordChangeView.as_view(), name='password_change'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
